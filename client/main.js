286992ddfc448f0acab0ef2df3a9fc6ec4aefa51
sergiy <sergiychemodanov@gmail.com>
2015-05-29 21:41:23 -0700

    added headline

--- client/main.js
+++ client/main.js
@@ -1,1 +1,13 @@
 //Events and Helpers
+Template.survey.helpers({
+	getValue: function(){
+		switch(getElementById('slider').value()){
+			case 0:
+				return "Never";
+			case 50:
+				return "Sometimes";
+			case 100:
+				return "Always";
+		}
+	}
+})
